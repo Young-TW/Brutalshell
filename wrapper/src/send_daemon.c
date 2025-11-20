c101df9365cef602ab3acbe8eef79a6a5d6dcd35
Zhengbao Lai <laizhengbao@icloud.com>
2025-11-20 21:02:14 +0800

    feat: send to daemon

---
+++ wrapper/src/send_daemon.c
@@ -0,0 +1,40 @@
+
+#include "wrapper.h"
+
+#include <unistd.h>
+
+#include <string.h>
+#include <stdlib.h>
+
+int send_daemon ( int method, char *restrict session, int fd, char *restrict content, ssize_t len ){
+
+	register int ret = -1;
+
+	register char *buf;
+
+	static const char *restrict sep = ": ";
+
+	register size_t slen;
+
+	switch ( method ){
+		case 0:
+			slen = strlen( session ) + strlen( sep ) + len;
+			buf = malloc( slen );
+			strncpy(
+					strcpy(
+						strcpy( buf, session ),
+						sep
+					),
+					content,
+					slen
+			);
+			len = write( fd, buf, slen );
+			free( buf );
+
+			ret = slen != len;
+
+			break;
+	}
+
+	return ret;
+}
